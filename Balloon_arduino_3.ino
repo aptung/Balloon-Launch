c9546f57ab331c05dfdd10aa0de6e8c0a83746e5
aptung <70546542+aptung@users.noreply.github.com>
2022-03-10 22:54:49 -0800

    redo timing; various other cleaning up; NOT TESTED
    i might have messed up on the logic for the buzzer...

--- Balloon_arduino_3.ino
+++ Balloon_arduino_3.ino
@@ -1,52 +1,80 @@
 #include <math.h>
 
-// CODE FOR HEATING PAD, BUZZER
-const int heatingPin = 6; // put pin here
-const int tempSensor = A3; // temperature sensor
-const int buzzer = 9; //buzzer to arduino pin 9
+const int heatingPin = 6;
+const int tempSensorPin = A3;
+const int buzzerPin = 9;
+
+unsigned long timeTemperature;
+unsigned long timeBuzzer;
+
+double temperature_c;
+int temperature_val;
+
 int minTemp = -10; // in C
 int maxTemp = 10;
+
 int cycle = 0;
 
+
 void setup() {
-  // Initialize Serial Monitor
   Serial.begin(9600);
 
-  // Set heating pin out
-  pinMode(heatingPin, OUTPUT); 
+  pinMode(heatingPin, OUTPUT);
+  pinMode(tempSensorPin, INPUT);
+  pinMode(buzzerPin, OUTPUT);
 
-  pinMode(tempSensor, INPUT);
-
-  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
-
+  timeTemperature = millis();
+  timeBuzzer = millis();
 }
 
 void loop() {
-  // Read temperature from the sensor 
-  int val = analogRead(tempSensor); // digital read gives # from 0 - 1023
-  Serial.print(val);
-  Serial.print(": ");
+  
+  if (millis()-timeTemperature>1000){
+    timeTemperature = millis();
+    getTemperature();
+  }
+  
+  if (millis()-timeBuzzer>1000){
+    timeBuzzer = millis();
+    buzzer();
+  }
+}
+
+void getTemperature() {
+  temperature_val = analogRead(tempSensorPin);
+//  Serial.print("Temperature voltage= ");
+//  Serial.println(temperatureVal);
   
   // t actual = -133 + 25.3 ln val
-  //double temperature_c = -133 + 25.3 * log(val);
-  double temperature_c = -30;
+  temperature_c = -133 + 25.3 * log(temperature_val);
+
+  Serial.print("(");
+  Serial.print(millis());
+  Serial.print(")");
+
+  Serial.print("Temperature (C) = ");
   Serial.println(temperature_c);
-  
-  // CODE FOR HEATING PAD
+}
+
+void adjustHeating() {
   if (temperature_c > maxTemp) {
     digitalWrite(heatingPin, LOW);
   }
   if (temperature_c < minTemp) {
     digitalWrite(heatingPin, HIGH);
-    Serial.print("hello");
-  } // END CODE FOR HEATING PAD
-
-  if (cycle == 10) {
-    cycle = 0;
-    tone(buzzer, 3000); // optimal frequency as determined by testing that made grace (and everyone else) want to die
-    delay(1000);        // ...for 1 sec
-    noTone(buzzer);     // Stop sound...
   }
-  cycle++;
-  delay(1000);
 }
+
+void buzzer(){
+  if (cycle == 10) {
+      cycle = 0;
+      tone(buzzerPin, 3000); // optimal frequency as determined by testing that made grace (and everyone else) want to die
+    }
+    else if (cycle==0){
+      noTone(buzzerPin);
+      cycle++;
+    }
+    else{
+      cycle++;
+    }
+  }
