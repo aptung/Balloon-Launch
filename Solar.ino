7cd2bc05df4b4f0a8e83297c18b9a6aa369cbb96
Grace Tang <48929887+gractang@users.noreply.github.com>
2022-03-14 10:52:51 -0700

    updated w millis over delay()

--- Solar.ino
+++ Solar.ino
@@ -1,14 +1,22 @@
-const int solar = A11; // solar panel
-const int filter = A7; // solar panel with UV filter
+const int solar = A2; // solar panel
+const int filter = A3; // solar panel with UV filter
+unsigned long timeCurrent;
+int del = 1000;
 
 void setup(){
   pinMode(solar, INPUT); 
   pinMode(filter, INPUT); 
+  timeCurrent = millis();
   Serial.begin(9600);
 
 }
 
 void loop(){
+  if (millis()-timeCurrent>del) { // Repeat the thing inside every del millis
+    timeCurrent = millis(); // reset the time
+    Serial.print("Time = ");
+    Serial.println(timeCurrent);
+    
     int val = analogRead(solar);
     Serial.print(val);
     double volt = val / 1023.0 * 5; // scale based on arduino 5V
@@ -20,8 +28,8 @@ void loop(){
     double fvolt = fval / 1023.0 * 5;
     Serial.print(" (filter) : ");
     Serial.println(fvolt);
+  }
     
-    delay(1000);
     
   
 }
