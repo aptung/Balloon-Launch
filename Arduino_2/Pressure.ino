dc3ef60cef9398b5258355b182411b831b905ae5
aptung <70546542+aptung@users.noreply.github.com>
2022-03-19 21:20:37 -0700

    copied from arduino 1 code

--- Arduino_2/Pressure.ino
+++ Arduino_2/Pressure.ino
@@ -1,23 +1,5 @@
-// Thanks to Mike Grusin for letting me borrow the functions below from
-// the BMP180 example code.
-
-double sealevel(double P, double A)
-// Given a pressure P (mbar) taken at a specific altitude (meters),
-// return the equivalent pressure (mbar) at sea level.
-// This produces pressure readings that can be used for weather measurements.
-{
-  return (P / pow(1 - (A / 44330.0), 5.255));
-}
-
-
-double altitudeCalculator(double P, double P0)
-// Given a pressure measurement P (mbar) and the pressure at a baseline P0 (mbar),
-// return altitude (meters) above baseline.
-{
-  return (44330.0 * (1 - pow(P / P0, 1 / 5.255)));
-}
-
 void getPressure(){
+
   // To measure to higher degrees of precision use the following sensor settings:
   // ADC_256
   // ADC_512
@@ -28,9 +10,13 @@ void getPressure(){
   // Pressure is in mbar.
   pressure_abs = sensor.getPressure(ADC_2048);
   pressure_corrected = (pressure_abs+9.96392)/1.00249; // Correction based on calibration testing
+
+  Serial.print("(");
+  Serial.print(millis());
+  Serial.print(")");
   
-  Serial.print("Pressure raw (mbar)= ");
-  Serial.println(pressure_abs);
+//  Serial.print("Pressure raw (mbar)= ");
+//  Serial.println(pressure_abs);
 
   Serial.print("Pressure corrected (mbar)= ");
   Serial.println(pressure_corrected);
