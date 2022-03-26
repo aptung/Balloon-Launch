a969cc7c4a925f44020e4f1611e341bff72dc5dd
aptung <70546542+aptung@users.noreply.github.com>
2022-03-25 20:32:05 -0700

    used an arraylist instead of an array

--- Balloon.java
+++ Balloon.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Balloon {
@@ -8,66 +9,73 @@ public class Balloon {
 
 	public static void main(String args[]) {
 		
-		// Stores time in first position, pressure in second position
-		double[][] pressureData = new double[100000][2];
-		
-		
-		// Time in first position, pressure in second, temp in third
-		double[][] tempData = new double[100000][3];
-		
-		
-		
-		int counterP = 0;
-		int counterT = 0;
+		ArrayList<double[]> pressureData = new ArrayList<double[]>(); // time in 1st pos, pressure in 2nd pos
+		ArrayList<double[]> tempData = new ArrayList<double[]>();
+		ArrayList<double[]> altitudeData = new ArrayList<double[]>();
+//		// Stores time in first position, pressure in second position
+//		double[][] pressureData = new double[100000][2];
+//		
+//		
+//		// Time in first position, pressure in second, temp in third
+//		double[][] tempData = new double[100000][3];
+//		
+//		
+//		
+//		int counterP = 0;
+//		int counterT = 0;
 
-		double pressure = 0; // Pressure is outside the loop and gets updated periodically
-		double time = 0;
-		double temp = 0;
+		// These are outside the loop and get updated periodically
+		double time = 0; // in sec
+		double pressure = 0; // in mbar
+		double temp = 0; // in deg C
+		double altitude = 0; // in m
 		
 		while (CONSOLE.hasNextLine()) {
 			String input = CONSOLE.nextLine();
 			if (input.length()==0) {
 				break;
-			} // When input is over, break
+			} // Output being over is indicated with a blank line, in which case we say bye
 			
 			time = Double.parseDouble(input.substring(input.indexOf("(")+1, input.indexOf(")")))/1000;
+			
 			// Find the lines that give pressure data
 			if (input.substring(input.indexOf(")")+1, input.indexOf(")")+2).equals("P")){
-				double oldPressure=pressure;
 				pressure = Double.parseDouble(input.substring(input.indexOf("=")+1));
 				if (pressure<=0 || pressure>1500) { // Filters out weird values
-					pressure = oldPressure;
+					pressure = pressureData.get(pressureData.size()-1)[1]; // then just take the previous reading
 				}
-				pressureData[counterP][0] = time;
-				pressureData[counterP][1] = pressure;
-				counterP = counterP+1;
+				pressureData.add(new double[] {time, pressure});
 			}
 			
 			// Find the lines that give the temp data
 			if (input.substring(input.indexOf(")")+1, input.indexOf(")")+2).equals("T")){
 				temp = Double.parseDouble(input.substring(input.indexOf("=")+1));
-				tempData[counterT][0] = time;
-				tempData[counterT][1] = pressure;
-				tempData[counterT][2] = temp;
-				counterT++;
+				if (temp<=-200 || temp>100) { // Filters out weird values
+					temp = tempData.get(tempData.size()-1)[1]; // then just take the previous reading
+				}
+				tempData.add(new double[] {time, temp});
 				
+			}
+			
+			// Find the lines with GPS data
+			if (input.substring(input.indexOf(")")+1, input.indexOf(")")+2).equals("L")){
+				System.out.println(input);
+				altitude = Double.parseDouble(input.substring(input.indexOf("Alt")+5, input.indexOf("(mm)")))/1000;
+//				if (altitude<=-200 || altitude>100) { // Filters out weird values
+//					altitude = altitudeData.get(altitudeData.size()-1)[1]; // then just take the previous reading
+//				}
+				altitudeData.add(new double[] {time, altitude});
 			}
 			
 		}
 		
-		System.out.println("Time:");
-		for (int i=0; i<tempData.length; i++) {
-			if (tempData[i][0]!=0) {
-				System.out.println(tempData[i][0]);
-			}
+		System.out.println("Times");
+		for (int i=0; i<altitudeData.size(); i++) {
+			System.out.println(altitudeData.get(i)[0]);
 		}
-		
-		System.out.println("------");
-		System.out.println("Corresponding temperature:");
-		for (int i=0; i<tempData.length; i++) {
-			if (tempData[i][2]!=0) {
-				System.out.println(tempData[i][2]);
-			}
+		System.out.println("Altitudes");
+		for (int i=0; i<altitudeData.size(); i++) {
+			System.out.println(altitudeData.get(i)[1]);
 		}
 		
 	}
