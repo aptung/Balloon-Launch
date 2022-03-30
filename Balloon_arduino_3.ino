68956af88231f6522aacf815d92c6c718c5cf90f
aptung <70546542+aptung@users.noreply.github.com>
2022-03-30 11:38:47 -0700

    added comments

--- Balloon_arduino_3.ino
+++ Balloon_arduino_3.ino
@@ -1,28 +1,37 @@
-#include <math.h>
+#include <math.h> // For doing math in arduino
 
+
+// Create variables to store results (these are updated periodically)
+double temperature_c; // Actual temperature, in C
+int temperatureVal; // For storing the voltage that was read in
+
+// These are unsigned to increase the range
+// Each of these is updated at different times for each component, so that the timing
+// for each component is independent
+unsigned long timeTemperature, timeBuzzer;
+
+// Pin labels
 const int heatingPin = 6;
 const int tempSensorPin = A3;
 const int buzzerPin = 9;
 
-unsigned long timeTemperature;
-unsigned long timeBuzzer;
-
-double temperature_c;
-int temperature_val;
-
-int minTemp = -10; // in C
+// Thresholds for adjusting the heating pad, in C
+int minTemp = -10;
 int maxTemp = 10;
 
+// Used for timing the buzzer's on/off
 int cycle = 0;
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // Initialize Serial Monitor
 
+  // Initialize pins
   pinMode(heatingPin, OUTPUT);
   pinMode(tempSensorPin, INPUT);
   pinMode(buzzerPin, OUTPUT);
 
+  // Start timers
   timeTemperature = millis();
   timeBuzzer = millis();
 }
@@ -40,13 +49,11 @@ void loop() {
   }
 }
 
-void getTemperature() {
-  temperature_val = analogRead(tempSensorPin);
-//  Serial.print("Temperature voltage= ");
-//  Serial.println(temperatureVal);
+void getTemperature(){
+  temperatureVal = analogRead(tempSensorPin);
   
-  // t actual = -133 + 25.3 ln val
-  temperature_c = -133 + 25.3 * log(temperature_val);
+  // Conversion from voltage level to temperature (C)
+  temperature_c = -133 + 25.3 * log(temperatureVal);
 
   Serial.print("(");
   Serial.print(millis());
@@ -57,6 +64,8 @@ void getTemperature() {
 }
 
 void adjustHeating() {
+  // Note that maxTemp =/= minTemp to prevent the heating pad 
+  // from switching on and off quickly when the temp is near the threshold
   if (temperature_c > maxTemp) {
     digitalWrite(heatingPin, LOW);
   }
@@ -66,9 +75,10 @@ void adjustHeating() {
 }
 
 void buzzer(){
+  // "cycle" controls when the buzzer is on/off, namely every 10th time this function is called
   if (cycle == 10) {
     cycle = 0;
-    tone(buzzerPin, 3000); // optimal frequency as determined by testing that made grace (and everyone else) want to die
+    tone(buzzerPin, 3000); // "optimal" (i.e. most annoying) frequency as determined by testing
   }
   else if (cycle==0){
     noTone(buzzerPin);
