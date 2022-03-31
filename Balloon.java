9c3e268a8337cbab6ffbab8eb7c882ffd15b711a
aptung <70546542+aptung@users.noreply.github.com>
2022-03-31 13:40:05 -0700

    added speed calculations

--- Balloon.java
+++ Balloon.java
@@ -5,7 +5,7 @@ public class Balloon {
 	public static Scanner CONSOLE = new Scanner(System.in);
 
 	public static void main(String args[]) {
-		arduino2();
+		arduino1();
 	}
 	
 	public static void arduino2() {
@@ -78,6 +78,8 @@ public class Balloon {
 		ArrayList<double[]> tempData = new ArrayList<double[]>();
 		ArrayList<double[]> altitudeData = new ArrayList<double[]>();
 		ArrayList<double[]> positionData = new ArrayList<double[]>(); // time, lat, long
+		
+		ArrayList<double[]> speedData = new ArrayList<double[]>(); // time, speed (m/s)
 
 		// These are outside the loop and get updated periodically
 		double time = 0; // in sec
@@ -131,13 +133,56 @@ public class Balloon {
 
 		}
 
-		System.out.println("Time");
-		for (int i = 0; i < pressureData.size(); i++){
-			System.out.println(pressureData.get(i)[0]);
+		
+		for (int i=0; i<positionData.size()-1; i++) {
+			double lat1 = positionData.get(i)[1];
+			double lat2 = positionData.get(i+1)[1];
+			double lon1 = positionData.get(i)[2];
+			double lon2 = positionData.get(i+1)[2];
+			double el1 = altitudeData.get(i)[1];
+			double el2 = altitudeData.get(i+1)[1];
+			double distance = distance(lat1, lat2, lon1, lon2, el1, el2);
+			double timeElapsed = positionData.get(i+1)[0]-positionData.get(i)[0];
+			double timeMid = (positionData.get(i+1)[0]+positionData.get(i)[0])*0.5;
+			if (distance/timeElapsed>1000) {
+				speedData.add(new double[] {timeMid, speedData.get(i-1)[1]});
+			}
+			else {
+				speedData.add(new double[] {timeMid, distance/timeElapsed});
+			}
 		}
-		System.out.println("Pressure");
-		for (int i = 0; i < pressureData.size(); i++){
-			System.out.println(pressureData.get(i)[1]);
+		
+		System.out.println("Time (s)");
+		for (int i = 0; i < speedData.size(); i++){
+			System.out.println(speedData.get(i)[0]);
 		}
+		System.out.println("Speed (m/s)");
+		for (int i = 0; i < speedData.size(); i++){
+			System.out.println(speedData.get(i)[1]);
+		}
+	}
+	
+	
+	// Most upvoted answer here https://stackoverflow.com/questions/3694380/calculating-distance-between-two-points-using-latitude-longitude
+	// Uses Haversine method
+	// Returns distance in meters
+	public static double distance(double lat1, double lat2, double lon1,
+	        double lon2, double el1, double el2) {
+
+	    final int R = 6371; // Radius of the earth
+
+	    double latDistance = Math.toRadians(lat2 - lat1);
+	    double lonDistance = Math.toRadians(lon2 - lon1);
+	    double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+	            + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+	            * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+	    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	    double distance = R * c * 1000; // convert to meters
+
+	    double height = el1 - el2;
+
+	    distance = Math.pow(distance, 2) + Math.pow(height, 2);
+
+	    return Math.sqrt(distance);
 	}
 }
